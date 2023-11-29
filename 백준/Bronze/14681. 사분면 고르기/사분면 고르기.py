db2bcd002d78dcf80461e233b19d3c0a8b2ebe09
J-Yeah <105379441+J-Yeah@users.noreply.github.com>
2023-11-29 16:26:13 +0900

    [Bronze V] Title: 사분면 고르기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,14 @@
+a=int(input())
+
+if a<0 :
+    b=int(input())
+    if b<0 :
+        print(3)
+    else :
+        print(2)
+else :
+    b=int(input())
+    if b<0 :
+        print(4)
+    else :
+        print(1)
