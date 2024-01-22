75d7cb77cf176f63bfc1be20073e80389ce0caf9
J-Yeah <105379441+J-Yeah@users.noreply.github.com>
2024-01-22 10:20:26 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    int a,b;
+    
+    while (1){
+        scanf("%d %d", &a, &b);
+        if (a==0&&b==0){
+            return 0;
+        }
+        
+        if (a>b){
+            printf("Yes\n");
+        }
+        else {
+            printf("No\n");
+        }
+    }
+}
