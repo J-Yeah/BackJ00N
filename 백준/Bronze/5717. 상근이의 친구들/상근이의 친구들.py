9631079399c76a0edc7227009908bc54046a66f9
J-Yeah <105379441+J-Yeah@users.noreply.github.com>
2024-02-27 04:10:29 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.py
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.py
@@ -1,7 +1,7 @@
 import sys
 
 while True :
-    a, b = map(int, sys.stdin.readline().split())
+    a,b = map(int, sys.stdin.readline().split())
     if a==0 and b==0 :
         break
     print(a+b)
