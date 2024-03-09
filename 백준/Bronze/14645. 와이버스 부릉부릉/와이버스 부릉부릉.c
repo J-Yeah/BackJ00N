59d99ef92e48fe6bce8f36b52c632adf8af90857
J-Yeah <105379441+J-Yeah@users.noreply.github.com>
2024-03-10 05:52:06 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+	int a,b,num1,num2;
+	scanf("%d %d", &a, &b);
+
+	for (int i=1;i<=a;i++){
+		scanf("%d %d", &num1, &num2);
+	}
+
+	printf("비와이");
+	return 0;
+}
