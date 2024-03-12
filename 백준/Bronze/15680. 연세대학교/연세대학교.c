b2cc9e1cf19a904103a1482f8b8d56f1b95a89cb
J-Yeah <105379441+J-Yeah@users.noreply.github.com>
2024-03-13 06:31:28 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+	int num;
+	scanf("%d", &num);
+
+	if (num==0) {
+		printf("YONSEI");
+	}
+	else if (num==1) {
+		printf("Leading the Way to the Future");
+	}
+	
+	return 0;
+}
