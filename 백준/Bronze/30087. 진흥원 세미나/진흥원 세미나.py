9085f1d4b6d10abfc0c33003116a8523d58bd909
J-Yeah <105379441+J-Yeah@users.noreply.github.com>
2024-04-15 21:49:53 -0400

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,19 @@
+import sys
+num = int(sys.stdin.readline())
+
+for i in range(num):
+    a = input()
+    if a=="Algorithm" :
+        print("204")
+    elif a=="DataAnalysis" :
+        print("207")
+    elif a=="ArtificialIntelligence" :
+        print("302")
+    elif a=="CyberSecurity" :
+        print("B101")
+    elif a=="Network" :
+        print("303")
+    elif a=="Startup" :
+        print("501")
+    elif a=="TestStrategy" :
+        print("105")
