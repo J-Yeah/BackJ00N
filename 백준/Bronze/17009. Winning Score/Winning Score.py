8dc480b5371f71750d3ddb9fa74423708337b45b
J-Yeah <105379441+J-Yeah@users.noreply.github.com>
2025-04-06 22:01:53 -0400

    [Bronze IV] Title: Winning Score, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/17009. Winning Score/Winning Score.py
@@ -0,0 +1,14 @@
+a1 = int(input())
+a2 = int(input())
+a3 = int(input())
+b1 = int(input())
+b2 = int(input())
+b3 = int(input())
+a = a1*3 + a2*2 + a3
+b = b1*3 + b2*2 + b3
+if a > b :
+    print("A")
+elif b > a:
+    print("B")
+else :
+    print("T")
