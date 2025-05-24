9156bb5f39b67444b694a4de72cc20ec4413cce0
J-Yeah <105379441+J-Yeah@users.noreply.github.com>
2025-05-23 23:30:06 -0400

    [Bronze IV] Title: Yalda, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/33191. Yalda/Yalda.py
@@ -0,0 +1,12 @@
+ma = int(input())
+w = int(input())
+p = int(input())
+n = int(input())
+if ma >= w :
+    print("Watermelon")
+elif ma >= p :
+    print("Pomegranates")
+elif ma >= n :
+    print("Nuts")
+else :
+    print("Nothing")
