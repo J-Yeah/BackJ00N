acfaa8a2fd3b2a079517284bd5272d574d0c7d48
J-Yeah <105379441+J-Yeah@users.noreply.github.com>
2025-07-24 17:34:12 -0400

    [Bronze IV] Title: Triple Sevens, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/31518. Triple Sevens/Triple Sevens.py
@@ -0,0 +1,8 @@
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+c = list(map(int, input().split()))
+if a.count(7) and b.count(7) and c.count(7) :
+    print(777)
+else :
+    print(0)
