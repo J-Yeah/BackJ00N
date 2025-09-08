a981cb3af1c0986fcb4ac96b61df96a6f71b56aa
J-Yeah <105379441+J-Yeah@users.noreply.github.com>
2025-09-08 12:01:28 -0400

    [Bronze IV] Title: Простая задача, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/22155. Простая задача/Простая задача.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+for _ in range(int(input())):
+    i, f = map(int, input().split())
+    if (i <= 1 and f <= 2) or (i <= 2 and f <= 1):
+        print('Yes')
+    else:
+        print('No')
