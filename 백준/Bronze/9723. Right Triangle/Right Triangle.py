f2898aa1dc92115a797cdd5522ee249b25b1a140
J-Yeah <105379441+J-Yeah@users.noreply.github.com>
2025-10-21 19:29:17 -0400

    [Bronze III] Title: Right Triangle, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9723. Right Triangle/Right Triangle.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+for i in range(int(input())):
+    a = list(map(int,input().split()))
+    a.sort()
+    if a[2]**2 == a[0]**2 + a[1]**2 :
+        print(f"Case #{i+1}: YES")
+    else :
+        print(f"Case #{i+1}: NO")
