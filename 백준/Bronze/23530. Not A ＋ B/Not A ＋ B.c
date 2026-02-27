11e53f2d11c9e3b5edfd1d71a3cada548f82c7d1
J-Yeah <105379441+J-Yeah@users.noreply.github.com>
2026-02-27 16:37:36 +0900

    [Bronze III] Title: Not A + B, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/23530. Not A ＋ B/Not A ＋ B.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,n1,n2;
+    scanf("%d",&n);
+    for(int i=0; i<n; ++i)
+    { 
+        scanf("%d %d",&n1,&n2);
+        printf("1\n");
+    }
+    return 0;
+}
